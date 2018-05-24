01a9ef6a252bb115c2715a04c3c1394a131bdd5f
Jonathan Duran <headjd@gmail.com>
2018-05-23 22:57:57 -0400

    Componente Header

---
+++ src/components/home/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-jumbotron src="/img/vue-cine.jpeg" dark>
+        <v-container fill-height>
+            <v-layout align-center>
+                <v-flex>
+                    <h3 class="display-3">Bienvenido a VueCine</h3>
+                    <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis
+                        cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+                    <v-divider class="my-3"></v-divider>
+                    <div class="title mb-3">Descubre todas las mejores películas en cartelera!</div>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-jumbotron>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
