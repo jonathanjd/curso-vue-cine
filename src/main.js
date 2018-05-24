bf9b9fae234e2b2eb48eceb6feb59e06300504d3
Jonathan Duran <headjd@gmail.com>
2018-05-23 21:32:57 -0400

    Instalación de Librería

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+Vue.use(Vuetify)
+
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
