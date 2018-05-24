f5d58dc28fd961746e5a8926a3cf82ece593fc1c
Jonathan Duran <headjd@gmail.com>
2018-05-23 23:59:23 -0400

    Componente Body

---
+++ src/components/home/Body.vue
@@ -0,0 +1,247 @@
+<template>
+    <v-container grid-list-xs>
+        <v-layout row wrap class="mt-4 mb-4">
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+            <v-flex xs4>
+                <v-card class="mx-2 mb-1">
+                    <v-card-media class="white--text" height="200px" src="/img/vue-cine.jpeg">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline">Nombre Película</span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-card-media>
+                    <v-card-title>
+                        <div>
+                            <span class="grey--text">Año: 2018</span>
+                            <br>
+                            <span>Género: Acción</span>
+                            <br>
+                            <span>Duración: 145min</span>
+                        </div>
+                    </v-card-title>
+                    <v-card-actions>
+                        <v-btn flat color="primary">Compartir</v-btn>
+                        <v-btn flat color="purple">Más Información</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
